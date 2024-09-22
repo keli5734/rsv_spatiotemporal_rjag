f1e4cf40d7aa7a5438a7c8159bd6f162281d2a4b
keli5734 <li.k1@student.unimelb.edu.au>
2024-09-22 15:45:57 -0400

    add libs

---
+++ aggragate data.R
@@ -0,0 +1,30 @@
+# load packages 
+{
+library(sf)
+library(ggplot2)
+library(dplyr)
+library(tigris)
+library(viridisLite)
+library(zctaCrosswalk)
+library(zipcodeR)
+library(spatstat)
+library(spdep)
+
+library(arrow)
+
+
+library(spatstat)
+
+library(usmap)
+library(scales)
+#library(spatstat)
+
+vircol <-  viridis_pal()(100)
+
+library(duckdb)
+library(plotly)
+library(lubridate)
+}
+
+
+
